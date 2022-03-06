736315c6d3e442ec6e8ec41ea806006b175f507d
Antonello Lobianco <antonello@lobianco.org>
2022-03-06 12:07:19 +0100

    Grammar improvements in the exercises of Lesson JULIA2

--- lessonsMaterial/02_JULIA2/forestExercise/forestExercise.jl
+++ lessonsMaterial/02_JULIA2/forestExercise/forestExercise.jl
@@ -1,29 +1,31 @@
 ################################################################################
 # forestGrowthFitting Problem
 #
-# The objective of this problem is to use the so-called "raw data" that the National Forest Inventory of France make availmable at the level of the individual inventoried trees and plots to fit a geric growth model of the forest stands in terms of volumes with respect to the trees age.
+# How fast are French forests growing? How much timber can they provide in one hectare?
+# Surely forests provide multiple ecosystem services, but in this (simplified) exercise we focus on the above question by looking at the so-called "raw data" from the French National Forest Inventory, both in terms of individual trees and in terms of inventoried plots, to fit a generic growth model of the forest stands in terms of volumes with respect to the age of the trees.
 #
 
 # ### Environment set-up and data loading
 
-# Start by setting the working directory to the directory of this file and activate it. If you have the `Manifest.toml` and `ProjecT.toml` files in the directory, run `instantiate()`, otherwise manually add the packages Pipe, HTTP, CSV, DataFrames, LsqFit, StatsPlots.
+# 1) Start by setting the working directory to the directory of this file and activate it. If you have the provided `Manifest.toml` file in the directory, just run `Pkg.instantiate()`, otherwise manually add the packages Pipe, HTTP, CSV, DataFrames, LsqFit, StatsPlots.
 
-# Load the packages Pipe, HTTP, CSV, DataFrames, LsqFit, StatsPlots
+# 2) Load the packages Pipe, HTTP, CSV, DataFrames, LsqFit, StatsPlots
 
-# Load from internet or from local files the following data:
+# 3) Load from internet or from local files the following data:
 
-ltURL = "https://github.com/sylvaticus/IntroSPMLJuliaCourse/blob/main/lessonsMaterial/02_JULIA2/forestExercise/data/arbres_foret_2012.csv?raw=true" # live individual trees data
-dtURL = "https://github.com/sylvaticus/IntroSPMLJuliaCourse/blob/main/lessonsMaterial/02_JULIA2/forestExercise/data/arbres_morts_foret_2012.csv?raw=true" # dead individual trees data
+ltURL     = "https://github.com/sylvaticus/IntroSPMLJuliaCourse/blob/main/lessonsMaterial/02_JULIA2/forestExercise/data/arbres_foret_2012.csv?raw=true" # live individual trees data
+dtURL     = "https://github.com/sylvaticus/IntroSPMLJuliaCourse/blob/main/lessonsMaterial/02_JULIA2/forestExercise/data/arbres_morts_foret_2012.csv?raw=true" # dead individual trees data
 pointsURL = "https://github.com/sylvaticus/IntroSPMLJuliaCourse/blob/main/lessonsMaterial/02_JULIA2/forestExercise/data/placettes_foret_2012.csv?raw=true" # plot level data
-docURL = "https://github.com/sylvaticus/IntroSPMLJuliaCourse/blob/main/lessonsMaterial/02_JULIA2/forestExercise/data/documentation_2012.csv?raw=true" # optional, needed for the species label
+docURL    = "https://github.com/sylvaticus/IntroSPMLJuliaCourse/blob/main/lessonsMaterial/02_JULIA2/forestExercise/data/documentation_2012.csv?raw=true" # optional, needed for the species label
 
-# If you go for downloading the data from internet, make for each of them a `@pipe` macro from `HTTP.get(URL).body` to `CSV.File(_)` to dfinally DataFrame
+# If you choosen to download the data from internet, you can make for each of the dataset a `@pipe` macro starting with `HTTP.get(URL).body`, continuing the pipe with `CSV.File(_)` and end the pipe with a DataFrame object.
 
-# Out of all the variables in these dataset, select only for the `lt` and `dt` dataframes the columns "idp" (pixel id),"c13" (circumference ad 1.30 meters) and "v" (tree's volume). Vertical concatenate the two dataset in a overall `trees` dataset.
-# For the `points` dataset, select only the variables "idp" (pixel id), "esspre" (code of the main forest species in the stand) and "cac" (age class)?
+# 4) These datasets have many variable we are not using in this exercise.
+# Out of all the variables, select only for the `lt` and `dt` dataframes the columns "idp" (pixel id), "c13" (circumference at 1.30 meters) and "v" (tree's volume). Then vertical concatenate the two dataset in an overall `trees` dataset.
+# For the `points` dataset, select only the variables "idp" (pixel id), "esspre" (code of the main forest species in the stand) and "cac" (age class).
 
 
-# Define the following function to compute the contribution of each tree to the volume per hectar measure of the plot
+# 5) As the French inventory system is based on a concentric sample method (small trees are sampled on a small area (6 metres radius), intermediate trees on a concentric area of 9 metres and only large trees (with a circonference larger than 117.5 cm) are sampled on a concentric area of 15 metres of radius), define the following function to compute the contribution of each tree to the volume per hectare:
 
 """
     vHaContribution(volume,circonference)
@@ -42,39 +44,37 @@ function vHaContribution(v,c13)
         return v/(15^2*pi/(100*100))
     end
 end
+# Use the above function to compute `trees.vHa` based on `trees.v` and `trees.c13`.
 
-# Use the above function to compute `trees.vHa`
-# Aggregate the `trees` dataframe by the `idp` column to retrieve the sum of `vHa` and the number of trees for each point, callign these two columns `vHa` and `ntrees`.
-# Join this aggregated "by point" dataframe with the original points dataframe using the column `idp`.
+# 6) Aggregate the `trees` dataframe by the `idp` column to retrieve the sum of `vHa` and the number of trees for each point, calling these two columns `vHa` and `ntrees`.
 
-# Use boolean selection to apply the following filters:
+# 7) Join the output of step 6 (the trees dataframe aggregated "by point") with the original points dataframe using the column `idp`.
+
+# 8) Use boolean selection to apply the following filters:
 filter_nTrees           = points.ntrees .> 5
 filter_IHaveAgeClass    = .! in.(points.cac,Ref(["AA","NR"]))
 filter_IHaveMainSpecies = .! ismissing.(points.esspre) 
 filter_overall          = filter_nTrees .&& filter_IHaveAgeClass .&& filter_IHaveMainSpecies
 
-# Run the followinf line to parse the age class (originally as a string indicating the 5-ages group) as an integer and computing the mid-range of the class in years. For example, class "02" will become 7.5 anni.
-
+# 9) Run the following line to parse the age class (originally as a string indicating the 5-ages group) to an integer and compute the mid-range of the class in years. For example, class "02" will become 7.5 years.
 points.cac              = (parse.(Int64,points.cac) .- 1 ) .* 5 .+ 2.5
 
-# Define the following logistic model of the growth relation with respect to the age with 3 parameters and make its vectorised form:
+# 10) Define the following logistic model of the growth relation with respect to the age with 3 parameters and make its vectorised form:
 logisticModel(age,parameters) = parameters[1]/(1+exp(-parameters[2] * (age-parameters[3]) ))
-
 logisticModelVec(age,parameters) = # .... complete
 
-# Set `initialParameters` to 1000,0.05 and 50 respectivelly
+# 11) Set `initialParameters` to 1000,0.05 and 50 respectivelly
 
-# Perform the fittin of the model using the function `curve_fit(model,X,Y,initial parameters)` and obtain the fitted parameter fitobject.param
+# 12) Perform the fittin of the model using the function `curve_fit(model,X,Y,initial parameters)` and obtain the fitted parameter fitobject.param
 
-# Compute the standard error for each estimated parameter and the confidence interval at 10% significance level
+# 13) Compute the standard error for each estimated parameter and the confidence interval at 10% significance level
 
-# Plot a chart of fitted (y) by stand age (X) (i.e. the logisticModel with the given parameters):
+# 14) Plot a chart of fitted (y) by stand age (x) (i.e. the logisticModel with the given parameters)
 
-# Add to the plot a scatter chart of the actual observed VHa:
+# 15) Add to the plot a scatter chart of the actual observed VHa
 
-
-# ### Optional part: looking to the species individually..
-# Try to look and plot the 5 most common species and display their specific plot
+# 16) [OPTIONAL] Look at the growth curves of individual species.
+# Try to perform the above analysis for individual species, fur example plot the fitted curves for the 5 most common species
 
 
 
--- lessonsMaterial/02_JULIA2/loggingOptimisation/loggingOptimisation.jl
+++ lessonsMaterial/02_JULIA2/loggingOptimisation/loggingOptimisation.jl
@@ -1,22 +1,24 @@
 ################################################################################
 #    loggingOptimisation Problem
 #
-# The objective of this problem is to find the optimal level of activities to perform to maximise the company profit, given (a) the profitability (gross margin) of each activity, (b) the resources available to the company and (c) the technical coefficients that links each activity to the resource needed (positive coefficient) or provided (negative coefficient).
-#
-# The problem is depicted in the SpreadSheet file "Optimal production mix.ods" and can be solved there by using the "Solver tool" as indicated in that file. You can use it to check your results.
+# You are the decision-maker of a productive unit (here a logging company) and have a set of possible activities to undergo.
+# How do you choose the activities that lead to a maximisation of the unit's profit? 
+# The objective of this problem is hence to find the optimal level of activities to maximise the company profit, given (a) the profitability (gross margin) of each activity, (b) the resources available to the company and (c) the matrix of technical coefficients that link each activity to the resources needed (positive coefficient) or provided (negative coefficient) by that specific activity.
+
+# The problem is the same as those in the SpreadSheet file "Optimal production mix.ods" (available in the `data` folder) and can be solved in LibreOffice by using the "Solver tool" as indicated in that file. You can use it to check your results.
 #
 #
 
 # ### Environment set-up and data loading
 
-# Start by setting the working directory to the directory of this file and activate it. If you have the `Manifest.toml` and `ProjecT.toml` files in the directory, run `instantiate()`, otherwise manually add the packages JuMP, GLPK, DataFrames, CSV, Pipe, HTTP.
+# 1) Start by setting the working directory to the directory of this file and activate it. If you have the `Manifest.toml` file in the directory, just run `Pkg.instantiate()`, otherwise manually add the packages JuMP, GLPK, DataFrames, CSV, Pipe, HTTP.
 
-# Load the packages using DelimitedFiles, JuMP, GLPK, DataFrames, CSV, Pipe, HTTP
+# 2) Load the packages DelimitedFiles, JuMP, GLPK, DataFrames, CSV, Pipe, HTTP
 
-# Load from internet or from local files the following data:
+# 3) Load from internet or from local files the following data:
 
-urlActivities = "https://raw.githubusercontent.com/sylvaticus/IntroSPMLJuliaCourse/main/lessonsMaterial/02_JULIA2/loggingOptimisation/data/activities.csv"
-urlResources = "https://raw.githubusercontent.com/sylvaticus/IntroSPMLJuliaCourse/main/lessonsMaterial/02_JULIA2/loggingOptimisation/data/resources.csv"
+urlActivities   = "https://raw.githubusercontent.com/sylvaticus/IntroSPMLJuliaCourse/main/lessonsMaterial/02_JULIA2/loggingOptimisation/data/activities.csv"
+urlResources    = "https://raw.githubusercontent.com/sylvaticus/IntroSPMLJuliaCourse/main/lessonsMaterial/02_JULIA2/loggingOptimisation/data/resources.csv"
 urlCoefficients = "https://raw.githubusercontent.com/sylvaticus/IntroSPMLJuliaCourse/main/lessonsMaterial/02_JULIA2/loggingOptimisation/data/coefficients.csv"
 
 # The result must be:
@@ -27,35 +29,37 @@ urlCoefficients = "https://raw.githubusercontent.com/sylvaticus/IntroSPMLJuliaCo
 # For example to download from internet and import the matrix you can use something like:
 coef  = @pipe HTTP.get(urlCoefficients).body |> readdlm(_,';')
 
-# Determine `nA` and `nR` as the number of activities and the number of resources (use the `size` function)
+# 4) Determine `nA` and `nR` as the number of activities and the number of resources (use the `size` function)
 
 
 # ### Optimisation model definition
 
-# Define `profitModel` as a model to be optimised using the `GLPK.Optimizer`
-# Optional: set `GLPK.GLP_MSG_ALL` as the `msg_lev` of GLPK
+# 5) Define `profitModel` as a model to be optimised using the `GLPK.Optimizer`
+# 6) [OPTIONAL] set `GLPK.GLP_MSG_ALL` as the `msg_lev` of GLPK
 
 # ### Model's endogenous variables definition
 
-# Define the non-negative model `x` variable, indexed by the positions between 1 and `nA` (i.e. `x[1:nA] >= 0`) (you could use the @variables macro)
+# 7) Define the non-negative model `x` variable, indexed by the positions between 1 and `nA` (i.e. `x[1:nA] >= 0`) (you could use the @variables macro). Don't set the `x` variable to be integer at this step, as some variables are continuous, just set them to be non-negative.
 
-# Set the variables for which the corresponding `integer` column in the `activity` dataframe is equal to 1 as a integer variable.
+# 8) Set the variables for which the corresponding `integer` column in the `activity` dataframe is equal to 1 as a integer variable.
 # To set the specific vaciable `x[a]` as integer use  `set_integer(x[a])`
 
 # ### Model's constraint definition
 
-# Define the `resLimit[r in 1:nR]` contraint(s), such that when you sum `coef[r,a]*x[a]` for all the `1:nA` activities you have a value lower than `resources.initial[r]`
+# 9) Define the `resLimit[r in 1:nR]` family of contraints, such that when you sum `coef[r,a]*x[a]` for all the `1:nA` activities you must have a value not greater than `resources.initial[r]`
 
 # ### Objective definition
 
-# Define the objective as the maximisation of the profit given by summing for each of the `1:nA` activities `activities.gm[a] * x[a]`
+# 10) Define the objective as the maximisation of the profit given by summing for each of the `1:nA` activities `activities.gm[a] * x[a]`
 
 # ### Model resolution
-# (optionally) print the model to check it and optimize if. Also, check with the function `status = termination_status(profitModel)` that the status is `OPTIMAL`.
+# 11) [OPTIONAL] Print the model to check it
+# 12) Optimize the model
+# 13) Check with the function `status = termination_status(profitModel)` that the status is `OPTIMAL` (it should be!)
 
 # ### Print optimal level of activities
 
-# Run the following code to print the results (optimal level of activities)
+# 14) Run the following code to print the results (optimal level of activities)
 
 if (status == MOI.OPTIMAL || status == MOI.LOCALLY_SOLVED || status == MOI.TIME_LIMIT) && has_values(profitModel)
     println("#################################################################")
@@ -82,7 +86,6 @@ else
 end
 
 
-# ### OPTIONAL: Observe the emergence of scale effects
-
-# Optionally re-run the model with `initial2` initial resources instead of `initial` and notice how this bigger company can afford differnt kind of activities (logging high forest instead of coppices in this example) obtaining a better profitability per unit of resource emploied.
-# Can you guess which are the aspects of the optimisation model that allow for the emergence of these scale effects ?
+# 15) [OPTIONAL] Observe the emergence of scale effects
+# Optionally re-run the model with `initial2` initial resources (instead of `initial`) and notice how this larger company can afford to perform different types of activities (logging high forest instead of coppices in this example) and obtain a better profitability per unit of resource employed.
+# Can you guess which are the aspects of the optimisation model that allow for the emergence of these scale effects?
